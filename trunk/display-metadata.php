65724df6bf21d87323aec723a1b990fb4cf73b4c
Manuel Canga <dev@trasweb.net>
2020-12-17 15:08:04 +0100

    feat(plugin): Instance plugin class on start of plugin

--- trunk/display-metadata.php
+++ trunk/display-metadata.php
@@ -1,11 +1,12 @@
-<?php
-declare( strict_types = 1 );
+<?php declare( strict_types = 1 );
+
+namespace Trasweb\Plugins\DisplayMetadata;
 
 /*
  * Plugin Name: Display Metadata
  * Plugin URI: https://github.com/trasweb/DisplayMetadata
  * Description: Show post/term/user properties and metadata in a metabox in its creation/edition form.
-  * Version: 0.1
+ * Version: 0.1
  * Author: Manuel Canga
  * Author URI: https://manuelcanga.dev
  * License: GPL3
@@ -14,10 +15,18 @@ declare( strict_types = 1 );
  * Domain Path: /languages
 */
 
-if ( ! defined( "ABSPATH" ) ) {
+if ( !defined( "ABSPATH" ) ) {
 	die( "Hello, World!" );
 }
 
-if ( ! is_admin() ) {
+if ( !is_admin() ) {
 	return;
 }
+
+( function () {
+	require __DIR__ . '/class-plugin.php';
+
+	${'display-metadata'} = new Plugin( $_GET ?: [] );
+
+	add_action( 'admin_init', ${'display-metadata'} );
+} )();
