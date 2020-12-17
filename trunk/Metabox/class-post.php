65633b67a49822c05a5ff9dd6e8a9871d8526403
Manuel Canga <dev@trasweb.net>
2020-12-17 15:16:39 +0100

    feat(plugin): Add a Post class of metaboxes

---
+++ trunk/Metabox/class-post.php
@@ -0,0 +1,74 @@
+<?php declare( strict_types = 1 );
+
+namespace Trasweb\Plugins\DisplayMetadata\Metabox;
+
+use Trasweb\Plugins\DisplayMetadata\Plugin;
+use const ARRAY_A;
+
+/**
+ * This class manages `Display Metadata` post metabox.
+ */
+final class Post extends Metabox {
+
+	protected const TITLE       = 'Post information';
+
+	protected const HEADER_FILE = '';
+
+	protected const FOOTER_FILE = '';
+
+	/**
+	 * Register a metabox in order to display it later.
+	 *
+	 * @return void
+	 */
+	public function register(): void {
+		if ( !$this->can_be_registered() ) {
+			return;
+		}
+
+		add_meta_box(
+			'trasweb_metadata_metabox',     // Unique ID
+			static::TITLE,                // Box title
+			[ $this, 'display' ],              // Content callback
+			$this->get_accepted_cpt()          // Post type
+		);
+	}
+
+	/**
+	 * Check if a metabox can be registered
+	 *
+	 * @return bool
+	 */
+	protected function can_be_registered(): bool {
+		$current_screen = Plugin::get_current_screen();
+
+		return 'post' === $current_screen->slug;
+	}
+
+	/**
+	 * Helepr: Retrieve list of post type with show-ui. This cpt are where the metabox will be displayed.
+	 *
+	 * @return array
+	 */
+	protected function get_accepted_cpt(): array {
+		return array_values( get_post_types( [ 'show_ui' => true ] ) ?: [] );
+	}
+
+	/**
+	 * Retrieve item properties/fields. E.g: ID, post_title, ...
+	 *
+	 * @return array
+	 */
+	protected function get_item_properties(): array {
+		return get_post( $this->item_id, ARRAY_A ) ?: [];
+	}
+
+	/**
+	 * Retrieve item metaata. E.g: _edit_lock
+	 *
+	 * @return array
+	 */
+	protected function get_item_metadata(): array {
+		return array_map( 'array_shift', get_post_meta( $this->item_id ) ?: [] );
+	}
+}
