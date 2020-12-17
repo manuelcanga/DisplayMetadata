97f7ad084429bd27abff71c0d219a833614009c2
Manuel Canga <dev@trasweb.net>
2020-12-17 23:07:34 +0100

    fix(views): Add support to NULL values

--- trunk/views/meta_list.php
+++ trunk/views/meta_list.php
@@ -1,21 +1,21 @@
 <table>
-	<tbody>
-	<?php
+   <tbody>
+   <?php
 
-	foreach ( $item_vars as $key => $value ): ?>
-		<tr>
-			<td class="meta_key"><?= $key ?></td>
-			<td class="meta_value">
-				<?php  if ( is_array( $value ) ) {
-				   $item_vars = $value;
-					include static::META_LIST_FILE;
-				}else {
-					echo htmlentities( $value );
-				}
-				?>
-			</td>
-		</tr><?php
-	endforeach;
-	?>
-	</tbody>
+   foreach ( $item_vars as $key => $value ): ?>
+      <tr>
+      <td class="meta_key"><?= $key ?></td>
+      <td class="meta_value">
+		  <?php if ( is_array( $value ) ) {
+			  $item_vars = $value;
+			  include static::META_LIST_FILE;
+		  } else {
+			  echo is_null( $value ) ? 'NULL' : htmlentities( $value );
+		  }
+		  ?>
+      </td>
+      </tr><?php
+   endforeach;
+   ?>
+   </tbody>
 </table>
