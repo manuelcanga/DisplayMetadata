f30f746ddcd3a4c2582d02c59506d3b24bf3d44d
Manuel Canga <dev@trasweb.net>
2020-12-18 08:46:04 +0100

    feat(views): Allow to sort metadata keys before show them

--- trunk/views/meta_list.php
+++ trunk/views/meta_list.php
@@ -1,15 +1,15 @@
 <table>
    <tbody>
    <?php
-
    foreach ( $item_vars as $key => $value ): ?>
       <tr>
       <td class="meta_key"><?= $key ?></td>
       <td class="meta_value">
 		  <?php
-        $value = maybe_unserialize( $value );
-        if ( is_array( $value ) ) {
+		  $value = maybe_unserialize( $value );
+		  if ( is_array( $value ) ) {
 			  $item_vars = $value;
+			  ksort( $item_vars );
 			  include static::META_LIST_FILE;
 		  } else {
 			  echo is_null( $value ) ? 'NULL' : htmlentities( $value );
