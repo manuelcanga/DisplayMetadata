898ccaa1bcd85db26175261cd96040a29287699e
Manuel Canga <dev@trasweb.net>
2020-12-19 01:19:04 +0100

    feat(metabox): fit metabox to screen width

--- trunk/assets/display_metadata.js
+++ trunk/assets/display_metadata.js
@@ -1,20 +1,45 @@
+trasweb_fit_metabox_width = function(){
+    /* Avoid overflow */
+    var metadata_container =  document.getElementById('trasweb-metadata-metabox');
+
+    if( 0 === metadata_container.clientWidth ) {
+        /* Fix Gutenberg */
+        setTimeout('trasweb_fit_metabox_width()', 100);
+        return ;
+    }
+
+    for( var depth_i = 1; true; depth_i++ ) {
+        var main_tr_list = metadata_container.querySelectorAll('.depth_' + depth_i);
+
+        if( main_tr_list.length <= 0 ) {
+            break;
+        }
+
+        for( var i = 0; i < main_tr_list.length; i++ ) {
+            var top_container =  main_tr_list[i].parentElement.parentElement;
+            var td_key = main_tr_list[i].children[0];
+            var td_value  = main_tr_list[i].children[1];
+
+            if( ( td_value.clientWidth + td_key.clientWidth ) > top_container.clientWidth ) {
+                td_value.style.maxWidth = ( top_container.clientWidth - td_key.clientWidth - 15 /* padding */ ) + 'px';
+            }
+        }
+    }
+};
+
 /**
  * Show/Hide metabox when click in
  */
 document.addEventListener('DOMContentLoaded', function() {
+    /* non post container */
     var container =  document.getElementById('trasweb_metadata_metabox__container');
 
-    if( null != container && 'undefined' != container ) {
+    /* enable open/close */
+    if( null != container ) {
         container.getElementsByTagName('h2')[0].addEventListener('click', function(event) {
             this.classList.toggle('closed');
         }.bind(container));
     }
 
-    var top_container =  document.getElementById('trasweb-metadata-metabox');
-    var max_td_width = top_container.clientWidth / 3;
-    var td_list = top_container.querySelectorAll("#trasweb-metadata-metabox td.meta_value");
-
-    for (let i = 0; i < td_list.length; i++) {
-        td_list[i].style.maxWidth = max_td_width + 'px';
-    }
+    trasweb_fit_metabox_width();
 });
--- trunk/views/meta_list.php
+++ trunk/views/meta_list.php
@@ -1,16 +1,23 @@
+<?php
+$depth = isset($depth)? $depth : 1;
+?>
 <table>
    <tbody>
    <?php
    foreach ( $item_vars as $key => $value ): ?>
-      <tr>
+   <tr class="depth_<?= $depth ?>">
       <td class="meta_key"><?= $key ?></td>
       <td class="meta_value">
 		  <?php
+		  /** @TODO ITERATOR */
 		  $value = maybe_unserialize( $value );
 		  if ( is_array( $value ) ) {
-			  $item_vars = $value;
-			  ksort( $item_vars );
-			  include static::META_LIST_FILE;
+			  (function() use($depth, $value) {
+				  $depth++;
+				  $item_vars = $value;
+				  ksort( $item_vars );
+				  include static::META_LIST_FILE;
+			  })();
 		  } else {
 			  echo is_null( $value ) ? 'NULL' : htmlentities( $value );
 		  }
