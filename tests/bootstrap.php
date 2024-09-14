a6eb8668545b4133e3eb05744ccdd293cbbf2d18
Manuel Canga <info@trasweb.net>
2024-09-14 10:09:39 +0200

    tests: Add BasicConfigTest

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,13 @@
 <?php
-
 declare(strict_types=1);
 
+namespace Trasweb\Plugins\DisplayMetadata\Tests;
+
 $plugin_dir = __DIR__ . '/../trunk';
+/**
+ * @const \Trasweb\Plugins\DisplayMetadata\Tests\PLUGIN_DIR
+ */
+define(__NAMESPACE__.'\PLUGIN_DIR', $plugin_dir);
 
 $plugin_services = include $plugin_dir . '/config/services.conf.php';
 
