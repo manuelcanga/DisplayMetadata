c611a568d668847c8d97c155d5590447b09d4289
Manuel Canga <info@trasweb.net>
2024-09-14 12:29:57 +0200

    ci: allow to lint of config files and tests files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,4 +17,4 @@ $plugin_services = include $plugin_dir . '/config/services.conf.php';
 
 ${'display-metadata'} = $plugin_services['plugin'](base_dir: $plugin_dir, services: $plugin_services);
 
-// Throw tests with  vendor/bin/phpunit --bootstrap tests/bootstrap.php tests
+// Throw tests with  vendor/bin/phpunit --testdox
